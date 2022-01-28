c8e8c8b66e6cc7749db88a81730f1fbacaa08b52
Kim-JW <bbam03@naver.com>
2022-01-28 16:36:21 +0900

    tomcat plugin &deployment setting

--- fileupload/src/main/java/com/poscoict/fileupload/service/FileUploadService.java
+++ fileupload/src/main/java/com/poscoict/fileupload/service/FileUploadService.java
@@ -3,6 +3,7 @@ package com.poscoict.fileupload.service;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Calendar;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,7 +11,9 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FileUploadService {
 	
-	private static String SAVE_PATH = "/Users/gimjaeung/upload-images";
+	//private static String SAVE_PATH = "/Users/gimjaeung/upload-images";
+	private static String SAVE_PATH = "/upload-images";
+	private static String URL_BASE = "/images";
 	
 	public String restore(MultipartFile multipartFile) {
 		String url = null;
@@ -21,18 +24,43 @@ public class FileUploadService {
 			}
 			
 			String originFileName = multipartFile.getOriginalFilename();
+			String extName = originFileName.substring(originFileName.lastIndexOf('.')+1);
+			String saveFilename = generateSaveFilename(extName);
+			
 			long fileSize = multipartFile.getSize();
 			
 			System.out.println("##############" + originFileName);
+			System.out.println("##############" + extName);
+			System.out.println("##############" + saveFilename);
 			System.out.println("##############" + fileSize);
 			
 			byte[] data = multipartFile.getBytes();
-			OutputStream os = new FileOutputStream(SAVE_PATH + "");
+			OutputStream os = new FileOutputStream(SAVE_PATH + "/" + saveFilename);
+			os.write(data);
+			os.close();
+			
+			url = URL_BASE + "/" + saveFilename;
 			
 		} catch (IOException e) {
 			throw new RuntimeException("file upload error:" + e);
 		}
 		
 		return url;
+	}
+
+	private String generateSaveFilename(String extName) {
+		String filename = "";
+		Calendar calendar = Calendar.getInstance();
+		
+		filename += calendar.get(Calendar.YEAR);
+		filename += calendar.get(Calendar.MONTH);
+		filename += calendar.get(Calendar.DATE);
+		filename += calendar.get(Calendar.HOUR);
+		filename += calendar.get(Calendar.MINUTE);
+		filename += calendar.get(Calendar.SECOND);
+		filename += calendar.get(Calendar.MILLISECOND);
+		filename += ("." + extName);
+		
+		return filename;
 	}
 }
